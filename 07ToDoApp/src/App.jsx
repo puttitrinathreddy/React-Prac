e523f699285c3e8521eb6177c9db12c918e6ba62
Trinath Reddy <reddytrinath39@gmail.com>
2024-02-04 00:44:18 +0530

    changes done in tailwind

--- 07ToDoApp/src/App.jsx
+++ 07ToDoApp/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
     >
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#172842] rounded-2xl shadow-lg min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
